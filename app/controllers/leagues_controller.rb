d0a3bb85276a62ea04875a2919725c98e49be821
Matthieu Harbonnier <matthieuharbonnier@Air-de-Matthieu.lan>
2023-10-11 10:04:51 +0200

    league model controller and view for index

---
+++ app/controllers/leagues_controller.rb
@@ -0,0 +1,7 @@
+class LeaguesController < ApplicationController
+
+	def index
+		@leagues=League.all
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
   root "standalonepages#homepage"
+  resources :leagues, only: [:index]
 
 end
