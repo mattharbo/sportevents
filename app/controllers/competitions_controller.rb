1d193a7bca67fe0016fc75cc1395f0350ae63c02
Matthieu Harbonnier <matthieuharbonnier@Air-de-Matthieu.lan>
2023-10-12 22:20:05 +0200

    competition create, update, and destroy controller functions

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,7 +1,55 @@
 class CompetitionsController < ApplicationController
+
+  before_action :get_all_leagues_and_seasons, only: [:edit, :new]
+  before_action :set_competition, only: [:show, :edit, :update, :destroy]
   
   def index
     @competitions=Competition.all
+  end
+
+  def new
+    @competition=Competition.new
+  end
+
+  def edit
+  end
+
+  def create
+    target_season=Season.find(params[:competition][:season_id])
+    target_league=League.find(params[:competition][:league_id])
+    Competition.create(season:target_season, league:target_league)
+    redirect_to competitions_path
+  end
+
+  def update
+    target_season=Season.find(params[:competition][:season_id])
+    target_league=League.find(params[:competition][:league_id])
+    @competition.update(season:target_season, league:target_league)
+    redirect_to competitions_path
+  end
+
+  def destroy
+    @competition.destroy
+    redirect_to competitions_path
+  end
+
+# ****
+# **** Private function starting from here
+# ****
+
+  private
+
+  def get_all_leagues_and_seasons
+    @leagues=League.all
+    @seasons=Season.all
+  end
+
+  def set_competition
+    @competition = Competition.find(params[:id])
+  end
+
+  def competition_params
+    params.require(:competition).permit(:league, :season)    
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'teams/index'
-  get 'competitions/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -8,7 +6,7 @@ Rails.application.routes.draw do
   root "standalonepages#homepage"
   resources :leagues, only: [:index,:new,:create,:show,:edit,:update,:destroy]
   resources :seasons, only: [:index,:new,:create,:show,:edit,:update,:destroy]
-  resources :competitions, only: [:index]
+  resources :competitions, only: [:index,:new,:create,:show,:edit,:update,:destroy]
   resources :teams, only: [:index,:new,:create,:show,:edit,:update,:destroy]
 
 end
